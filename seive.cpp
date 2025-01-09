9776cc292056201715c75b10277100aaf3d7522e
Tahira Anee <121106531+TahiraAnee@users.noreply.github.com>
2025-01-09 09:16:56 +0600

    Update seive.cpp

--- seive.cpp
+++ seive.cpp
@@ -1,6 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+const int mx = 1e8 + 123;
+bitset<mx> is_prime;
+vector<ll> prime;
+void primeGen(int n)
+{
+    n += 100;
+    for (int i = 3; i <= n; i += 2)
+        is_prime[i] = 1;
+    ll sq = sqrt(n);
+    for (int i = 3; i <= sq; i += 2)
+    {
+        if (is_prime[i] == 1)
+        {
+            for (int j = i * i; j <= n; j += (i + i))
+            {
+                is_prime[j] = 0;
+            }
+        }
+    }
+    is_prime[2] = 1;
+    prime.push_back(2);
+    for (int i = 3; i <= n; i += 2)
+    {
+        if (is_prime[i] == 1)
+            prime.push_back(i);
+    }
+}
 int main()
 {
     int n;
